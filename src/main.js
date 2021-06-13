2049130dffeb349ee4e84002ba5eba964f27b23f
cube8540 <cube8540@gmail.com>
2021-06-13 20:34:30 +0900

    Vuex 추가

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import App from './App.vue'
 
 import vuetify from './plugins/vuetify'
 
+import store from '@/store'
+
 Vue.config.productionTip = false
 
 Vue.use(VueCompositionAPI)
-new Vue({ vuetify, render: h => h(App) }).$mount('#app')
+new Vue({
+    vuetify,
+    store,
+    render: h => h(App)
+}).$mount('#app')
