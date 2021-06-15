27eff9bb9da31cd657619701291ea5b9dab22b61
cube8540 <cube8540@gmail.com>
2021-06-15 23:59:58 +0900

    헤더 컴포넌트 추가

---
+++ src/components/headers/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app-bar app dark flat absolute color="primary">
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader'
+}
+</script>
+
+<style scoped>
+
+</style>
